81708fab7d579f6ede039706ca1d15051c729cc0
abbiebabu <abhaymaskey12@gmail.com>
2025-05-23 18:23:56 +0545

    changed tick to untick

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -2,11 +2,11 @@ import tick from "../assets/tick.png";
 import untick from "../assets/untick.png";
 import del from "../assets/del.png";
 
-export const TodoItems = ({text}) => {
+export const TodoItems = ({ text }) => {
   return (
     <div className="flex ml-3 my-8 items-center px-5 ">
       <div className="flex flex-1 gap-5 items-center cursor-pointer ">
-        <img className="h-10 w-10" src={tick} alt="" />
+        <img className="h-10 w-10" src={untick} alt="" />
         <p className="text-slate-200 text-[18px]">{text}</p>
       </div>
       <img src={del} alt="" className="w-6 cursor-pointer mr-4" />
